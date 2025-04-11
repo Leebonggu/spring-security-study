08185f13288b12ec620c76c52b711c4e954cea56
leebonggu <leebonggu12@hanmail.net>
2025-04-11 23:28:20 +0900

    test

--- src/main/java/personal/fcsecurity1/controller/HelloController.java
+++ src/main/java/personal/fcsecurity1/controller/HelloController.java
@@ -1,13 +1,22 @@
 package personal.fcsecurity1.controller;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import personal.fcsecurity1.user.DefaultUser;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/api/v1/hello")
     public String hello() {
+        UserDetails user = new DefaultUser();
+
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        System.out.println(user.getAuthorities());
+        System.out.println(user.isAccountNonExpired());
+
         return "Hello, World!";
     }
 }
---
+++ src/main/java/personal/fcsecurity1/user/DefaultUser.java
@@ -0,0 +1,43 @@
+package personal.fcsecurity1.user;
+
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class DefaultUser implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "READ");
+    }
+
+    @Override
+    public String getPassword() {
+        return "password";
+    }
+
+    @Override
+    public String getUsername() {
+        return "user";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
